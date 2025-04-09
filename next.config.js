d1237ae24edbfbd49408c3773811f2e1fd0622dc
ikwerre-dev <investorhonour@gmail.com>
2025-04-09 23:53:55 +0100

    Fix next-pwa config

--- next.config.js
+++ next.config.js
@@ -2,24 +2,13 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // Disable in development
-  // Prevent multiple generations
+  disable: process.env.NODE_ENV === 'development',
   buildExcludes: [/middleware-manifest\.json$/],
-  // Prevent webpack watch mode warnings
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config, {
-        watch: false,
-        watchOptions: {},
-      });
-    }
-    return config;
-  },
+
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your existing next config
 }
 
 module.exports = withPWA(nextConfig)
